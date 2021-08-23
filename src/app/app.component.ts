00804e1cd9a8f15ecc50b63277aa98ab25e7e02f
milos1mitrovic <milos1mitrovic@gmail.com>
2021-08-23 15:59:40 +0200

    third form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,10 @@ import {
 export class AppComponent implements OnInit {
   personalDetails!: FormGroup;
   addressDetails!: FormGroup;
-  educationalDetails!: FormGroup;
-  personal_step = false;
+  registerDetails!: FormGroup;
+  personal_step = true;
   address_step = false;
-  education_step = false;
+  register_step = false;
   step = 1;
 
   languages: string[] = ['Portugese', 'English'];
@@ -42,16 +42,17 @@ export class AppComponent implements OnInit {
       city: ['', Validators.required],
       zip: ['', Validators.required],
     });
-    this.educationalDetails = this.formBuilder.group({
-      highest_qualification: ['', Validators.required],
-      university: ['', Validators.required],
-      total_marks: ['', Validators.required],
+    this.registerDetails = this.formBuilder.group({
+      taxpayerNumber: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
     });
     console.log(this.personalDetails);
 
     if (this.step === 1) {
       this.personal_step = true;
     }
+    console.log(this.step);
   }
 
   get personal() {
@@ -60,45 +61,74 @@ export class AppComponent implements OnInit {
   get address() {
     return this.addressDetails.controls;
   }
-  get education() {
-    return this.educationalDetails.controls;
+  get register() {
+    return this.registerDetails.controls;
+  }
+
+  goTo(step: number, details: FormGroup) {
+    console.log(details);
+    if (details.invalid) {
+      console.log('Validation');
+      this.validateAllFormFields(details);
+      return;
+    }
+    this.step = step;
   }
 
   next() {
     if (this.step == 1) {
-      this.personal_step = true;
+      this.address_step = true;
       if (this.personalDetails.invalid) {
         this.validateAllFormFields(this.personalDetails);
+        console.log(this.step);
         return;
       }
-      this.step++;
-    }
-    if (this.step == 2) {
-      this.address_step = true;
+      this.step = 2;
+      console.log(this.step);
+    } else if (this.step == 2) {
+      this.register_step = true;
+
       if (this.addressDetails.invalid) {
+        this.validateAllFormFields(this.addressDetails);
         return;
       }
-      this.step++;
+      this.step = 3;
+      console.log(this.step);
     }
+    if (this.step == 3) {
+      console.log(this.step);
+      return;
+    }
+    console.log(this.step);
   }
   previous() {
     if (this.step == 1) {
-      this.personal_step = true;
       return;
     }
-    this.step--;
+
     if (this.step == 2) {
-      this.education_step = false;
+      this.step--;
+      this.personal_step = true;
+      this.address_step = false;
+      console.log(this.step);
+    }
+
+    if (this.step == 3) {
+      this.step--;
+      this.register_step = false;
+      this.address_step = true;
+      console.log(this.step);
     }
   }
   submit() {
     if (this.step == 3) {
-      this.education_step = true;
-      if (this.educationalDetails.invalid) {
+      this.register_step = true;
+      if (this.registerDetails.invalid) {
         console.log('INVALID');
         return;
       }
     }
+    console.log("form submitted")
   }
 
   validateAllFormFields(formGroup: FormGroup) {
